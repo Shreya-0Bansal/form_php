7bbfdc7403a69d2d19d0b6f74010368ae257ed39
SHREYA_BANSAL <112041707+Shreya-0Bansal@users.noreply.github.com>
2022-11-21 19:02:55 +0530

    original

--- git/assignment.php
+++ git/assignment.php
@@ -8,24 +8,23 @@ $file=$_FILES["Document"]["name"];
 $filesize=$_FILES["Document"]["size"];
 $filetype=pathinfo($file,PATHINFO_EXTENSION);
 $file_tmp=$_FILES["Document"]["tmp_name"];
-
 if ($filetype == "pdf") {
-    if ($filesize < 200000) {
-        if ($Input == "Aadhar Card") {
-            $actual_path = "Adhaar/" . $File;
-            move_uploaded_file($tempFile, $actual_path);
+    if ($filesize < 204800) {
+        if ($Input == "Aadhar") {
+            $actual_path = "Aadhar/".$file;
+            move_uploaded_file($file_tmp, $actual_path);
         }
         if ($Input == "License") {
-            $actual_path = "License/" . $File;
-            move_uploaded_file($tempFile, $actual_path);
+            $actual_path = "License/".$file;
+            move_uploaded_file($file_tmp, $actual_path);
         }
-        if ($Input == "Voter Id") {
-            $actual_path = "Voter/" . $File;
-            move_uploaded_file($tempFile, $actual_path);
+        if ($Input == "voter") {
+            $actual_path = "voter/".$file;
+            move_uploaded_file($file_tmp, $actual_path);
         }
         if ($Input == "Pan") {
-            $actual_path = "Pan/" . $File;
-            move_uploaded_file($tempFile, $actual_path);
+            $actual_path = "Pan/".$file;
+            move_uploaded_file($file_tmp, $actual_path);
         }
     } else {
         echo "File Size Exceeds!<br>";
@@ -50,29 +49,29 @@ if ($file_type == "jpeg" || $file_type== "png" || $file_type == "jpg") {
 }
 
 if ($file_type == "jpeg") {
-    echo '<img src="Image/" alt="Image" width="200px" height="200px" ><br>';
+    echo '<img src="Image/smile.jpeg" style="border:2px solid black;padding:5px;" alt="Image" height="100px" width="100px" ><br>';
 }
 if ($file_type == "jpg") {
-    echo '<img src="Image/" alt="Image" width="200px" height="200px"><br>';
+    echo '<img src="Image/smile.jpg" style="border:2px solid black; padding:5px;" alt="Image" height="100px" width="100px""><br>';
 }
 if ($file_type == "png") {
-    echo '<img src="Image/" alt="Image" width="200px" height="200px"><br>';
+    echo '<img src="Image/smile.png" style="border:2px solid black;padding:5px;" alt="Image" height="100px" width="100px""><br>';
 }
 echo "Name:-".$firstname." ".$lastname."<br>";
 echo "DOB:-".$calender."<br>";
 echo "Gender:-".$gender."<br>";
 echo "ID Proof Selected:-".$file."</br>";
-if ($Input == "Aadhar Card") {
-    echo '<a href="Adhaar/"><input type="submit" value="Download" /></a><br>';
+if ($Input == "Aadhar") {
+    echo '<a href="Aadhar/docs.pdf"><input type="submit" value="Download" /></a><br>';
 }
 if ($Input == "License") {
-    echo '<a href="License/"><input type="submit" value="Download" /></a><br>';
+    echo '<a href="License/docs.pdf"><input type="submit" value="Download" /></a><br>';
 }
-if ($Input == "Voter Id") {
+if ($Input == "voter") {
 
-    echo '<a href="Voter/"><input type="submit" value="Download" /></a><br>';
+    echo '<a href="voter/docs.pdf"><input type="submit" value="Download" /></a><br>';
 }
 if ($Input == "Pan") {
-    echo '<a href="Pan/"><input type="submit" value="Download" /></a><br>';
+    echo '<a href="Pan/docs.pdf"><input type="submit" value="Download" /></a><br>';
 }
 ?>
